228e4d6436f5ca8f8da7dec6f7a9d3bce80bc1ff
DANIEL HONORATO <danihono@icloud.com>
2025-06-03 20:39:44 -0300

    req12

--- analisemercado.py
+++ analisemercado.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from dotenv import load_dotenv
 from crewai import Agent, Task, Crew
-from crewai_tools import SerperDevTool, FileWriterTool, ScrapeWebsiteTool
+from crewai.tools import SerperDevTool, FileWriterTool, ScrapeWebsiteTool
 from docx import Document
 from docx.shared import Inches, Pt
 from docx.enum.style import WD_STYLE_TYPE
